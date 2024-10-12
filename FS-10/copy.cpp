0e275aa820f9e425462d5d8a6bdc031d626a52f7
mnavdev21 <miqonav@gmail.com>
2024-10-12 23:42:42 +0400

    Copy with Holes

--- FS-10/copy.cpp
+++ FS-10/copy.cpp
@@ -1,76 +1,86 @@
-#include <iostream>
 #include <fcntl.h>
+#include <stdio.h>
 #include <unistd.h>
-#include <cstring>
-#include <cerrno>
+#include <sys/types.h>
 #include <sys/stat.h>
+#include <errno.h>
+#include <string.h>
 
 #define BUFFER_SIZE 4096
 
-int main(int argc, char** argv) {
+int main(int argc, char *argv[]) {
     if (argc != 3) {
-	std::cout << errno;
+        return errno;
+    }
+
+    int src_fd = open(argv[1], O_RDONLY);
+    if (src_fd == -1) {
+        perror("open source file");
         return 1;
     }
 
-    const char* src_file = argv[1];
-    const char* dst_file = argv[2];
-
-    int src_fd = open(src_file, O_RDONLY);
-    if (src_fd < 0) {
-        return errno;
-    }
-
-    int dst_fd = open(dst_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-    if (dst_fd < 0) {
+    int dst_fd = open(argv[2], O_WRONLY);
+    if (dst_fd == -1) {
+        perror("open destination file");
         close(src_fd);
-        return errno;
+        return 1;
     }
 
-    off_t total_bytes = 0;
     off_t current = 0;
     off_t file_size = lseek(src_fd, 0, SEEK_END);
-    off_t hole_count = 0, data_count = 0;
-
+    off_t holes = 0, data = 0;
     char buffer[BUFFER_SIZE];
-    size_t bytes_read;
-    lseek(src_fd, -file_size, SEEK_END);
-    while (true) {
-	off_t data_start = lseek(src_fd, current, SEEK_DATA);
+
+    while (current < file_size) {
+        off_t data_start = lseek(src_fd, current, SEEK_DATA);
         if (data_start == -1) {
-            perror("lseek (SEEK_DATA)");
-            return 1;
+            return errno;
         }
-	
+
+        if (data_start > current) {
+            off_t hole_size = data_start - current;
+	    holes += hole_size;
+            lseek(dst_fd, hole_size, SEEK_CUR);
+        }
+
         off_t hole_start = lseek(src_fd, data_start, SEEK_HOLE);
         if (hole_start == -1) {
-	    perror("lseek (SEEK_HOLE)");
-            return 1;       
-    	}
-	
-        data_count += hole_start - data_start;
-        if (hole_start < file_size) {
-            off_t next_data = lseek(src_fd, hole_start, SEEK_DATA);
-            if (next_data == -1) {
-               perror("lseek (SEEK_DATA after SEEK_HOLE)");
-               return 1;
-            }
-       	    hole_count += next_data - hole_start;
-	    total_bytes += hole_count + data_count;
-       	    current = next_data;
-	    bytes_read = read(src_fd, buffer, total_bytes);
-	    write(dst_fd, buffer, total_bytes);
-   	 }
-	else {
-	   break;
-	}
+            return errno;
+        }
 
+        off_t bytes_to_copy = hole_start - data_start;
+	data += bytes_to_copy;
+        lseek(src_fd, data_start, SEEK_SET);
+
+        while (bytes_to_copy > 0) {
+            ssize_t bytes_read = read(src_fd, buffer, (bytes_to_copy > BUFFER_SIZE) ? BUFFER_SIZE : bytes_to_copy);
+            if (bytes_read == -1) {
+                perror("read");
+                close(src_fd);
+                close(dst_fd);
+                return errno;
+            }
+
+            ssize_t bytes_written = write(dst_fd, buffer, bytes_read);
+            if (bytes_written == -1) {
+                perror("write");
+                close(src_fd);
+                close(dst_fd);
+                return errno;
+            }
+
+            bytes_to_copy -= bytes_read;
+        }
+
+        current = hole_start;
     }
-	
+
+    printf("File copied successfully with holes.\n");
+    printf("Data_bytes: %lld\n", (long long)data);
+    printf("Hole_bytes: %lld\n", (long long)holes);
+
     close(src_fd);
     close(dst_fd);
-
-    std::cout << "Successfully copied " << total_bytes << " bytes (data: " << (long long)data_count << ", hole: " << (long long)hole_count << ").\n";
-
     return 0;
 }
+
